6c6f9719d842dc6fc62e79e3f191412de3d9d7b3
Yash Deshmukh <yashdeshmukh300@gmail.com>
2023-07-04 23:47:10 +0530

    Day 2 last commit

---
+++ src/com/yash/Day2/JumpGame.java
@@ -0,0 +1,17 @@
+package com.yash.Day2;
+
+public class JumpGame {
+    public static void main(String[] args) {
+
+    }
+    public boolean canJump(int[] nums) {
+        int goal = nums.length-1;
+
+        for (int i = nums.length-2; i>=0; i--){
+            if( i+nums[i] >= goal){
+                goal = i;
+            }
+        }
+        return goal==0;
+    }
+}

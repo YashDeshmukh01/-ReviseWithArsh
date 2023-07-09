9a888e55549a3b4fa8929aa122281663075dab24
Yash Deshmukh <yashdeshmukh300@gmail.com>
2023-07-09 20:33:04 +0530

    Day 7 commit 1

---
+++ src/com/yash/Day7/EqualArrayElements.java
@@ -0,0 +1,18 @@
+package com.yash.Day7;
+
+public class EqualArrayElements {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3};
+        System.out.println(minMoves(nums));
+    }
+    public static int minMoves(int[] nums) {
+        int n = nums.length;
+        int sum = 0;
+        int min = Integer.MAX_VALUE;
+        for (int i : nums) {
+            min  = Math.min(min, i);
+            sum+=i;
+        }
+        return (sum-n*min);
+    }
+}

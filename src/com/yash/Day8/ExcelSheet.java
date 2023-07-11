3b9c5b45c0822be6f7977c57f7ad173dec439755
Yash Deshmukh <yashdeshmukh300@gmail.com>
2023-07-11 23:40:04 +0530

    Day 9 Commit

--- src/com/yash/Day8/ExcelSheet.java
+++ src/com/yash/Day8/ExcelSheet.java
@@ -4,5 +4,14 @@ public class ExcelSheet {
     public static void main(String[] args) {
 
     }
-
+    public String convertToTitle(int col) {
+        StringBuilder sb = new StringBuilder();
+        while(col>0){
+            int r = (col-1) % 26;
+            sb.append((char)(r+'A'));
+            col = (col-1)/26;
+        }
+        return sb.reverse().toString();
+    }
 }
+
---
+++ src/com/yash/Day9/ReverseInteger.java
@@ -0,0 +1,21 @@
+package com.yash.Day9;
+
+public class ReverseInteger {
+    public static void main(String[] args) {
+
+    }
+    public int reverse(int x) {
+        long reversed = 0;
+        int pop;
+
+        while(x!=0){
+            pop = x%10;
+            x/=10;
+            reversed = (reversed*10)+pop;
+        }
+        if(reversed<Math.pow(-2,31) || reversed>Math.pow(2,31))
+            return 0;
+        else
+            return (int)reversed;
+    }
+}

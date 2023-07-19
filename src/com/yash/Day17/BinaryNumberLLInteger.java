593d5bbf9da6b710744a9225b45983f13c6aa303
Yash Deshmukh <yashdeshmukh300@gmail.com>
2023-07-19 10:53:30 +0530

    Day17 Starts

---
+++ src/com/yash/Day17/BinaryNumberLLInteger.java
@@ -0,0 +1,17 @@
+package com.yash.Day17;
+
+public class BinaryNumberLLInteger {
+
+    public static void main(String[] args) {
+
+    }
+//    public int getDecimalValue(ListNode head) {
+//        StringBuilder sb = new StringBuilder();
+//        ListNode temp = head;
+//        while(temp!=null){
+//            sb.append(temp.val);
+//            temp = temp.next;
+//        }
+//        return Integer.parseInt(sb.toString(),2);
+//    }
+}
